1633e5b021137e74197368eb4d81c1b8f7fde0d3
Maciej Suchecki <maciej@MacBook-Pro-Maciej.local>
2021-01-03 20:24:15 +0100

    add rock & paper moves

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 function printMessage(msg){
-	let div = document.createElement('div');
+    let div = document.createElement('div');
+    let computerMove = 'kamień';
+    let playerMove = 'papier';
 	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+    document.getElementById('messages').appendChild(div);
+    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to 'playerMove' , to wygrywasz');
 }
 
 function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+    document.getElementById('messages').innerHTML = '';
 }
