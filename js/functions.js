3d59c8a912e4611f71046634659fc7c8166bcb26
Maciej Suchecki <maciej@MacBook-Pro-Maciej.local>
2021-01-04 12:37:01 +0100

    done ver. 1.1

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,9 @@
 function printMessage(msg){
     let div = document.createElement('div');
-    let computerMove = 'kamień';
     let playerMove = 'papier';
     let randomFraction = Math.random(); // losowanie z zakresu 0 - 0.999
     let calculation = randomFraction * 3 + 1; // wprowadzenie losowania 1 - 4
     let roundNumber = Math.floor(calculation); // zaokrąglenie wyniku losowania w dół, wynik: 1-3 
-    let randomNumber = Math.floor(Math.random() * 3 + 1); //zapis 3 zmiennych i działań w jednym wierszu
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
     printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' , to wygrywasz');
